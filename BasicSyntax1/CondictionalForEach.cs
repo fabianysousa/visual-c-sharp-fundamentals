f9aa9fa8549a4e8396819fbf7ed84f23f8fc00b3
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:19:31 -0300

    Estrutura condicional ForEach

---
+++ BasicSyntax1/CondictionalForEach.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class CondictionalForEach
+    {
+        static void main(string[] args)
+        {
+            string[] professor = {"Leonardo", "Marcondes", "Ana"};
+            
+            foreach(string professor in nomes)
+            {
+                Console.WriteLine(professor);
+            }
+        }
+    }
+}
