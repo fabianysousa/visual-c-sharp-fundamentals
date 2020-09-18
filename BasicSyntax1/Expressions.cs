b18c32362b5ed3ad8bbc29be54761c64dee727c8
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:09:01 -0300

    Expressões com c#

---
+++ BasicSyntax1/Expressions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class Expressions
+    {
+        static void main(string[] args)
+        {
+            int preco, desconto, total;
+            preco = 100;
+            desconto = 15;
+            total = preco - desconto;
+        }
+    }
+}
