80d8f61b6c49b054765a0680a500e8afce3b8556
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:19:10 -0300

    Estrutura condicional Switch

---
+++ BasicSyntax1/CondictionalSwitch.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class CondictionalSwitch
+    {
+        static void main(string[] args)
+        {
+            string clima = Console.ReadLine();
+            switch (clima)
+            {
+                case "Bom":
+                    Console.WriteLine("Vamos à praia!!!");
+                    break;
+                case "Nublado":
+                    Console.WriteLine("Vamos assistir a um filme no cinema");
+                    break;
+                default:
+                    Console.WriteLine("O dia pode estar nublado");
+                    break;
+            }
+        }
+    }
+}
