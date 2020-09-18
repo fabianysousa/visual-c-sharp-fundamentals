1a5f5cad965b3a82d3b130e282a0330f0e3cd664
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:14:53 -0300

    Estrutura condicional IF

---
+++ BasicSyntax1/CondictionaIF.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class CondictionaIF
+    {
+        static void main(string[] args)
+        {
+            String clima ="Bom";
+            if(clima == "Bom")
+{
+                Console.WriteLine("Vamos à praia!!!");
+            }
+            Console.Write("O dia pode estar nublado");
+        }
+    }
+}
