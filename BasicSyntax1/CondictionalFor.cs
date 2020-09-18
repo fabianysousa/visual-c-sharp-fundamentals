b39a0c6ff9f0244662d811fffe79f5de41cbf54f
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:19:46 -0300

    Estrutura condicional For

---
+++ BasicSyntax1/CondictionalFor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class CondictionaFor
+    {
+        static void main(string[] args)
+        {
+            for(int a = 0; a < 5; a++)
+            {
+                Console.WriteLine("{0}",a);
+            }
+        }
+    }
+}
