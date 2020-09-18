a229da83b579528b3967e181b84624579f348923
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:15:49 -0300

    Estrutura condicional Else

---
+++ BasicSyntax1/CondictionaElse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class CondictionaElse
+    {
+        static void main(string[] args)
+        {
+            String clima = "Bom";
+            if (clima == "Bom")
+            {
+                Console.WriteLine("Vamos à praia!!!");
+            }
+            else
+            {
+                Console.WriteLine("O dia pode estar nublado");
+            }
+        }
+    }
+}
