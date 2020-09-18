44dd1b700b492cdb2ee0e7f53f1ab05aafcedbfc
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-18 19:14:22 -0300

    Estrutura condicional Do While

---
+++ BasicSyntax1/CondictionalDoWhile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicSyntax1
+{
+    class CondictionalDoWhile
+    {
+        static void main(string[] args)
+        {
+            int a = 0;
+            do
+            {
+                if(a == 9)
+                break;
+                Console.WriteLine(a);
+                a++;
+            }
+            while(a < 5);
+        }
+    }
+}
