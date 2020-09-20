8cfc4e291d05fc66f82559927515ef17a4d14c37
Fabiany Sousa <fabianydesousa@gmail.com>
2020-09-20 14:28:23 -0300

    commi

---
+++ Activity/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Activity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result, number;
+            string name;
+
+            name = Console.ReadLine();
+            number = Console.Read();
+
+            for( int i = 1; i < 3; i++ )
+            {
+                number += i;
+            }
+
+            Console.WriteLine(number);
+            result = number + Convert.ToInt32(name);
+            Console.WriteLine(result);
+
+
+        }
+    }
+}
