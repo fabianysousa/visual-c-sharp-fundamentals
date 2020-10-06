f5f9b8dc844275a1253972966a58fd89406db837
Fabiany Sousa <fabianydesousa@gmail.com>
2020-10-06 15:28:40 -0300

    Mapeando classes para tabelas em banco de dados

---
+++ Activity5/Activity5/Produto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity5
+{
+    [Table ("Produto")]
+    public class Produto
+    {
+        [Key]
+        public int codPorduto { get; set; }
+        [Column]
+        public string descricao { get; set; }
+        [Column]
+        public double valorUnitario { get; set; }
+    }
+}
---
+++ Activity5/Activity5/Vendedor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Activity5
+{
+    [Table("Vendedor")]
+    public class Vendedor
+    {
+        [Key]
+        public int codVendendor { get; set; }
+        [Column]
+        public string nomeVendedor { get; set; }
+        [Column]
+        public double metaDeVenda { get; set; }
+    }
+}
