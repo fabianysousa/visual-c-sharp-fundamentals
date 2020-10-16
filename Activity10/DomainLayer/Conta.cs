570e6c9231ca811e1e6f096e90535e62faab20e4
Fabiany Sousa <fabianydesousa@gmail.com>
2020-10-16 15:12:41 -0300

    Aplicando a metodologia DDD

---
+++ Activity10/DomainLayer/Conta.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainLayer
+{
+    public class Conta
+    {
+        public int numero;
+        public float saldo;
+        public float conta;
+        public Cliente cliente;
+        public void criarPedido(int numero, float saldo, Cliente cliente)
+        {
+            this.numero = numero;
+            this.saldo = saldo;
+            this.cliente = cliente;
+
+        }
+
+        public void depositar(float valor) 
+        {
+            this.saldo = valor;
+        }
+
+        public Boolean sacar(float valor)
+        {
+            if(this.saldo >= valor)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Activity10/UserInterface/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// As informações gerais sobre um assembly são controladas por
+// conjunto de atributos. Altere estes valores de atributo para modificar as informações
+// associadas a um assembly.
+[assembly: AssemblyTitle("UserInterface")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UserInterface")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Definir ComVisible como false torna os tipos neste assembly invisíveis
+// para componentes COM. Caso precise acessar um tipo neste assembly de
+// COM, defina o atributo ComVisible como true nesse tipo.
+[assembly: ComVisible(false)]
+
+// O GUID a seguir será destinado à ID de typelib se este projeto for exposto para COM
+[assembly: Guid("f2bf8402-ad7d-4b70-a51a-2dd4319d012b")]
+
+// As informações da versão de um assembly consistem nos quatro valores a seguir:
+//
+//      Versão Principal
+//      Versão Secundária 
+//      Número da Versão
+//      Revisão
+//
+// É possível especificar todos os valores ou usar como padrão os Números de Build e da Revisão
+// usando o "*" como mostrado abaixo:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
