a803e91db474cbb9407c48fc362da676ebc21d47
Fabiany Sousa <fabianydesousa@gmail.com>
2020-10-20 18:32:35 -0300

    Criando aplicação .Net Core em Docker

---
+++ AppDockerDotnet/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NetCore.Docker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string amigo = "Mateus";
+            Console.WriteLine("Olá " + amigo);
+        }
+    }
+}
